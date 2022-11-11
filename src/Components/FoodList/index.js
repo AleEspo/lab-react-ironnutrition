a90a612766ba5599b2489ea689266996266255b6
Alessadro Esposito Brescia <alessandro.esposito.brescia@gmail.com>
2022-11-11 12:15:35 -0300

    iteration 1 done

--- src/Components/FoodList/index.js
+++ src/Components/FoodList/index.js
@@ -7,25 +7,45 @@ export function FoodList (){
 
   const [food, setFood] = useState(foodList)
 
-        return (
-        <div>
-            {food.map((single) => 
-            <div>
-              <p>{single.name}</p>
-            </div>
-            )}
-      </div>
+    //     return (
+    //     <div>
+    //         {food.map((single) => 
+    //         <div>
+    //           <p>{single.name}</p>
+    //         </div>
+    //         )}
+    //   </div>
+    // )
+    return (
+      <>
+        <h1>Food List</h1>
+        <div className="FoodList"></div>
+        {food.map((single)=> {
+          return (
+         <div>
+           <p>{single.name}</p>
+           <img src={single.image} width={50} ket={single.name} alt="food image"/>
+         </div>
+          )
+        })
+        }
+      </>
     )
-
-      // food.map((single)=>{
-  //   return (
-  //     <div>
+  // return(
+  // <>
+  //   <h1>Food List</h1>
+  //   <div>
+  //     food.map((single )=> {
+  //       return (
+  //       <div>
   //         <p>{single.name}</p>
-  //         <img src={single.image} width={50} ket={single.name}/>
-  //     </div>
-  //   )
-  // })
-
+  //         <img src={single.image} width={50} ket={single.name} alt="food image"/>
+  //       </div>
+  //       )
+  //     })
+  //   </div>
+  // </>
+  // )
     // return (
     //     <div>
     //         {food.map((single) => 
